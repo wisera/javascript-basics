bfc604dd7aeb63d6008872451b2111e6b0a2ee5b
wisera <luansurfer@hotmail.com>
2021-11-25 14:34:59 -0300

    added basic variable exercise

--- variables.js
+++ variables.js
@@ -3,3 +3,24 @@
 let name = 'Luan'
 
 console.log(name)
+
+// Basic variable exercise
+
+let a = 'red';
+let b = 'blue';
+
+console.log(a);
+console.log(b);
+
+// The output will be red and blue
+
+// Make the console log first blue and then red w/out changing the order of the console.log calls
+
+let a = 'red';
+let b = 'blue';
+let c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
