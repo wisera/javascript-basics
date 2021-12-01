ee03147d990997c6a41274e34464bb955868f6c5
wisera <luansurfer@hotmail.com>
2021-12-01 19:20:29 -0300

    added adding arrays, tomanipulating arrays in array.js

--- array.js
+++ array.js
@@ -14,3 +14,22 @@ console.log(selectedColors[2]);
 console.log(selectedColors.length)
 
 console.log('The amount of items is ' + selectedColors.length)
+
+// Manipulating Arrays //
+
+// Here are some ways of adding elements to an array
+
+const numbers = [3,4]
+
+// Add elements to the end of the array
+numbers.push(5,6) // this method automatically adds the arguments to the back to the array
+console.log(numbers)
+
+// Add elements to the beginning of the array
+numbers.unshift(1,2) // this method automatically adds the arguments to the start of the array
+console.log(numbers)
+
+// Add elements to the middle of the array
+numbers.splice(2,0,'a','b') // the first argument is to select an index point, the second argument is the number of elements to delete, if 0, the next arguments are inserted into the index position
+console.log(numbers)
+
